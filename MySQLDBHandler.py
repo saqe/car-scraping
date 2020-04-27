d3d59e54bf4908271d7d3ae27120a6f7178131b9
SaqibAleem <saqib.py@gmail.com>
2020-04-27 07:06:23 +0500

    Files added

---
+++ MySQLDBHandler.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+class MySQLDBHandler:
+    def __init__(self,_host_,_user_,_passwd_,_database_):
+        super().__init__()
+        self.db_connection=mysql.connector.connect(
+            host=_host_,
+            user=_user_,
+            passwd=_passwd_,
+            database=_database_)
+        self.db_cursor=self.db_connection.cursor()
+
+        print("Connection built with database! ver: ",self.db_connection.get_server_info())
+
+    def is_connected(self) -> bool:
+        return self.db_connection.is_connected()
+    
+    def query(self,sql_query) -> None:
+        self.db_cursor.execute(sql_query)
+
+    def fetch_many(self,sql_query) -> list:
+        self.db_cursor.execute(sql_query)
+        return self.db_cursor.fetchall()
+    
+    def fetch_many_first(self,sql_query) -> list:
+        self.db_cursor.execute(sql_query)
+        return [x[0] for x in self.db_cursor.fetchall()]
+
+    def fetch_one(self,sql_query) -> tuple:
+        self.db_cursor.execute(sql_query)
+        return self.db_cursor.fetchone()
+        
+
+        
+
---
+++ main.py
@@ -0,0 +1,71 @@
+from CarDatabaseHandler import CarDatabaseHandler
+from Scraper import Scraper
+import os
+import logging
+from dotenv import load_dotenv
+load_dotenv()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+REQUEST_HEADER={
+  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+  'accept-language': 'en-US,en;q=0.9',
+  'cache-control': 'no-cache',
+  'pragma': 'no-cache',
+  'upgrade-insecure-requests': '1',
+  'Host': os.getenv('REQUEST_HOST'),
+#   'Cookie':os.getenv('REQUEST_COOKIES'),
+  'user-agent': os.getenv('REQUEST_USER_AGENT')
+}
+
+HEADER_FOR_DATA=['Marca', 'Gamma', 'Model', 'Versió', 'Color Exterior','Quilòmetres', 'Price', 'Preu Ara', 'Preu Abans', 'Potencia (CV)', 'Canvi','Tracció','Color Interior','Combustible','Data Primera Matriculació','Data Fabricació', 'Disponibilitat','Estat','Premium','Vists', 'FAVORITS','Company Name','Motor','Cubicatge','Potencia (KW)','Par Motor (NM)']
+
+CHANGE_VALUE_DICT={
+  'Versió':'Versio',
+  'Quilòmetres':'Quilometres',
+  'Potencia (CV)':'Potencia_CV',
+  'Tracció':'Traccio',
+  'Color Exterior':'Color_Exterior',
+  'Color Interior':'Color_Interior',
+  'Potencia (KW)':'Potencia_KW',
+  'Par Motor (NM)':'Par_Motor_NM',
+  'Data Primera Matriculació':'Data_Primera',
+  'Data Fabricació':'Data_Fabricacio',
+}
+
+cars_db=CarDatabaseHandler(
+    host=os.getenv('DATABASE_HOST'),
+    user=os.getenv('DATABASE_USER'),
+    passwd=os.getenv('DATABASE_PASS'),
+    database=os.getenv('DATABASE_NAME'))
+
+scraper=Scraper()
+scraper.set_request_header(REQUEST_HEADER)
+scraper.set_search_page_api(os.getenv('SEARCH_API'))
+scraper.set_main_url(os.getenv('BASE_URL'))
+scraper.set_database_header(HEADER_FOR_DATA)
+scraper.set_dict_value_change(CHANGE_VALUE_DICT)
+
+set_of_car_available_today=set()
+
+for page_number in range(1,115):
+    print(page_number)
+    list_of_cars=scraper.scrape_cars_list(page_number)
+    for car in list_of_cars:
+        dataDict=scraper.scrape_car_detail_from_search_result(car)
+        ref_no=int(dataDict['REF_NO'])
+        set_of_car_available_today.add(ref_no)
+
+        if cars_db.is_ref_no_exists(ref_no):
+            #If REF_NO Exists but the availbility status was DOWN.
+            if cars_db.is_ref_num_not_available(ref_no):
+                print(ref_no," is up again")
+                cars_db.putCarAvailablityUP(ref_no)
+                logger.info(str(ref_no)+' is available again, Refreshing Values in database')
+                dataDict=scraper.scrape_car_profile_information(dataDict)
+            
+        # updatePreExistData(dataDict)
+        # continue
+        # dataDict=extractCarInformation(dataDict)
+        # writeNewCarRow(dataDict)
+
